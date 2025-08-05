236b521f555202e304d9184b5a711931653a8f52
unknown <bringeshchowdavarapu@gmail.com>
2025-08-05 08:57:55 -0700

    debugging ci-cd setup

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = ({
 }) => {
   const [accountStatus, setAccountStatus] = useState('');
 
-  // Mock data (should replace with your actual data fetching logic)
+  // Mock data (you should replace with your actual data fetching logic)
   const mockData = {
     currentUser: {
       isFirstTime: true,
